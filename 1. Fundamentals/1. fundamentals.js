8b51828650b07f1d355879d8a3213b1903ec185b
ynw99 <yusufedu99@gmail.com>
2020-11-28 23:00:54 +0700

    learn WebGL Fundamentals

--- 1. Fundamentals/1. fundamentals.js
+++ 1. Fundamentals/1. fundamentals.js
@@ -1,3 +1,59 @@
+/* eslint no-console:0 consistent-return:0 */
+'use strict';
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
 function main() {
+    // Get A Webgl context
+    var canvas = document.getElementById('myCanvas');
+    var gl = canvas.getContext('webgl');
+    if (!gl) {
+        return;
+    }
+
+    // Get the strings for our GLSL shaders
+    var vertexShaderSource = document.getElementById('vertex-source');
+    var fragmentShaderSource = document.getElementById('fragment-source');
+
+    // create GLSL shaders, upload the GLSL source, compile the shaders
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link the two shaders into a program
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+
+    // Create a buffer and put three 2d clip space points in it
+    var positionBuffer = gl.createBuffer();
+
     
 }
+
+main();
